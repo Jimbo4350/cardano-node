5f79b24b615207eb3e1e6e0f3b3359830f5704f3
John Ky <john.ky@iohk.io>
2021-07-20 17:19:44 +1000

    HTML trace

--- cardano-cli/src/Cardano/CLI/Render.hs
+++ cardano-cli/src/Cardano/CLI/Render.hs
@@ -1,14 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Cardano.CLI.Render
   ( renderHtmlShowS
   ) where
 
 import Data.Function
+import Options.Applicative.Help.Ann
 import Prettyprinter
 import Prettyprinter.Render.Util.Panic
 import Text.Show
 
 import qualified Data.List as L
 import qualified Data.Text as T
+
+renderOpenAnn :: Ann -> ShowS
+renderOpenAnn ann = case ann of
+  AnnTrace s -> id
+    . ("<span trace=" <>)
+    . (show s <>)
+    . (">" <>)
+
+renderCloseAnn :: ShowS
+renderCloseAnn = (<> "</span>")
 
 -- | Render a 'SimpleDocStream' to a 'ShowS', useful to write 'Show' instances
 -- based on the prettyprinter.
@@ -17,15 +30,22 @@ import qualified Data.Text as T
 -- instance 'Show' MyType where
 --     'showsPrec' _ = 'renderHtmlShowS' . 'layoutPretty' 'defaultLayoutOptions' . 'pretty'
 -- @
-renderHtmlShowS :: SimpleDocStream ann -> ShowS
-renderHtmlShowS = go
+renderHtmlShowS :: SimpleDocStream Ann -> ShowS
+renderHtmlShowS ds = id
+  . ("<html>\n" <>)
+  . ("<body>\n" <>)
+  . ("<pre>\n" <>)
+  . (<> "\n</pre>")
+  . (<> "\n</body>")
+  . (<> "\n</html>")
+  . go ds
   where
-    go :: SimpleDocStream ann -> ShowS
+    go :: SimpleDocStream Ann -> ShowS
     go sds = case sds of
-      SFail        -> panicUncaughtFail
-      SEmpty       -> id
-      SChar c x    -> showChar c . go x
-      SText _l t x -> showString (T.unpack t) . go x
-      SLine i x    -> showString ('\n' : L.replicate i ' ') . go x
-      SAnnPush _ x -> go x
-      SAnnPop x    -> go x
+      SFail           -> panicUncaughtFail
+      SEmpty          -> id
+      SChar c x       -> showChar c . go x
+      SText _l t x    -> showString (T.unpack t) . go x
+      SLine i x       -> showString ('\n' : L.replicate i ' ') . go x
+      SAnnPush ann x  -> renderOpenAnn ann . go x
+      SAnnPop x       -> go x . renderCloseAnn
